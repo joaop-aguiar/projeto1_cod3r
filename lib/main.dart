3b861b612196f28e2a9880a765aa0f1934698d2e
João Paulo de Oliveira Chaves Aguiar <joaoaguiar@id.uff.br>
2021-04-23 21:32:22 -0300

    pontuação e frase personalizada

--- lib/main.dart
+++ lib/main.dart
@@ -66,7 +66,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
                   perguntas: _perguntas,
                   perguntaSelecionada: _perguntaSelecionada,
                   responder: _responder)
-              : Resultado()),
+              : Resultado(_pontuacaoTotal)),
     );
   }
 }
